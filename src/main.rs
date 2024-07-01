d0e12c6a3e435972f5df173984bdb0644990494f
wny <viniberizzi@gmail.com>
2024-07-01 18:08:02 -0300

    add dhcp

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use tf::network::config::INTERFACE_NAME;
 use tf::network::dhcp_server;
-use tf::network::fake_gateway::start_gateway_simulation;
 
 fn main() {
-    // std::thread::spawn(|| {
-    //     start_gateway_simulation(INTERFACE_NAME);
-    // });
     
     dhcp_server::run();
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -2,4 +2,3 @@ pub mod config;
 pub mod dhcp_server;
 mod dhcp;
 pub mod dhcp_lease;
-pub mod fake_gateway;
